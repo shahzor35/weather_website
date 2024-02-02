d3b92fa462a9cc775134d641e075fb262d55715a
shahzor35 <shahzor575@gmail.com>
2024-02-03 01:34:19 +0530

    change code for city

--- weather.py
+++ weather.py
@@ -14,8 +14,9 @@ def get_weatherdata():
     'appid': "2d395903ac3a9d88a3c25c5d2326d70a",
     'units': "metric"}
     response = requests.get(url, params= param)
+    city = data['name']
     data = response.json()
-    return f"data :{data}"
+    return f"data :{data}, city :{city}"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port= 5002)
